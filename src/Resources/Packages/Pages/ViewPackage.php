0539a74815240640118b7396ff0918309fe49200
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2026-02-17 20:16:26 -0300

    feat: add validateCredentials action to view page and table, hide when already validated

--- src/Resources/Packages/Pages/ViewPackage.php
+++ src/Resources/Packages/Pages/ViewPackage.php
@@ -2,9 +2,13 @@
 
 namespace JeffersonGoncalves\FilamentSatis\Resources\Packages\Pages;
 
+use Filament\Actions\Action;
 use Filament\Actions\EditAction;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ViewRecord;
 use JeffersonGoncalves\FilamentSatis\Resources\Packages\PackageResource;
+use JeffersonGoncalves\LaravelSatis\Actions\ValidatePackageCredentials;
+use JeffersonGoncalves\LaravelSatis\Models\Package;
 
 class ViewPackage extends ViewRecord
 {
@@ -18,6 +22,28 @@ class ViewPackage extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [
+            Action::make('validateCredentials')
+                ->label(__('filament-satis::package.actions.validate_credentials'))
+                ->icon('heroicon-o-check-badge')
+                ->requiresConfirmation()
+                ->visible(fn () => ! $this->getRecord()->getAttribute('is_credentials_validated'))
+                ->action(function () {
+                    /** @var Package $record */
+                    $record = $this->record;
+                    $result = app(ValidatePackageCredentials::class)->execute($record);
+
+                    if ($result) {
+                        Notification::make()
+                            ->title(__('filament-satis::package.notifications.credentials_valid'))
+                            ->success()
+                            ->send();
+                    } else {
+                        Notification::make()
+                            ->title(__('filament-satis::package.notifications.credentials_invalid'))
+                            ->danger()
+                            ->send();
+                    }
+                }),
             EditAction::make(),
         ];
     }
